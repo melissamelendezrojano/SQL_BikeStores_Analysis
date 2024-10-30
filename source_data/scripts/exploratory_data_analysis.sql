c4c46712e2048920d721bfb0e06e28dc46e88b02
melissamelendezrojano <mrojano.melissa@gmail.com>
2024-10-30 19:17:40 +0100

    sales and store performance analysis 1

--- source_data/scripts/exploratory_data_analysis.sql
+++ source_data/scripts/exploratory_data_analysis.sql
@@ -13,269 +13,194 @@
 
 */
 
+
 -- Exploratory Data Analysis
 
--- 1. Overview of each table:
+-- Sales and Store Performance Analysis
+-- •	Which stores have the highest total sales?
 
--- brands' table
+-- TOTAL SALES PER STORE
 SELECT
-	COUNT(*) AS num_brands
-FROM
-	brands;
-
-/*
-|num_brands|
-|----------|
-|9         |
-*/
-
-SELECT
-	*
-FROM
-	brands;
-
-/*
-| brand_id | brand_name   |
-|----------|--------------|
-| 1        | Electra      |
-| 2        | Haro         |
-| 3        | Heller       |
-| 4        | Pure Cycles  |
-| 5        | Ritchey      |
-| 6        | Strider      |
-| 7        | Sun Bicycles |
-| 8        | Surly        |
-| 9        | Trek         |
-*/
-
--- categories' table
-SELECT
-	COUNT(*) AS num_categories
-FROM
-	categories;
-    
-/*
-|num_categories|
-|--------------|
-|7             |
-*/
-
-SELECT
-	*
-FROM
-	categories;
-
-/*
-| category_id | category_name       |
-|-------------|---------------------|
-| 1           | Children Bicycles   |
-| 2           | Comfort Bicycles    |
-| 3           | Cruisers Bicycles   |
-| 4           | Cyclocross Bicycles |
-| 5           | Electric Bikes      |
-| 6           | Mountain Bikes      |
-| 7           | Road Bikes          |
-*/
-
--- stores' table
-SELECT
-	COUNT(*) AS num_stores
-FROM
-	stores;
-    
-/*
-|num_stores|
-|----------|
-|3         |
-*/
-
-SELECT
-	*
-FROM
-	stores;
-
-/*
-| store_id | store_name       | phone          | email                | street              | city       | state | zip_code |
-|----------|------------------|----------------|----------------------|---------------------|------------|-------|----------|
-| 1        | Santa Cruz Bikes | (831) 476-4321 | santacruz@bikes.shop | 3700 Portola Drive  | Santa Cruz | CA    | 95060    |
-| 2        | Baldwin Bikes    | (516) 379-8888 | baldwin@bikes.shop   | 4200 Chestnut Lane  | Baldwin    | NY    | 11432    |
-| 3        | Rowlett Bikes    | (972) 530-5555 | rowlett@bikes.shop   | 8000 Fairway Avenue | Rowlett    | TX    | 75088    |
-*/
-
--- products' table
-SELECT
-	COUNT(*) AS num_products
-FROM
-	products;
-    
-/*
-|num_products|
-|------------|
-|321         |
-*/
-
-SELECT
-	*
-FROM
-	products
-LIMIT
-	5;
-
-/*
-| product_id | product_name                       | brand_id | category_id | model_year | list_price |
-|------------|------------------------------------|----------|-------------|------------|------------|
-| 1          | Trek 820 - 2016                    | 9        | 6           | 2016       | 379.99     |
-| 2          | Ritchey Timberwolf Frameset - 2016 | 5        | 6           | 2016       | 749.99     |
-| 3          | Surly Wednesday Frameset - 2016    | 8        | 6           | 2016       | 999.99     |
-| 4          | Trek Fuel EX 8 29 - 2016           | 9        | 6           | 2016       | 2899.99    |
-| 5          | Heller Shagamaw Frame - 2016       | 3        | 6           | 2016       | 1320.99    |
-*/
-
--- orders' table
-SELECT
-	COUNT(*) AS num_orders
-FROM
-	orders;
-    
-/*
-|num_orders|
-|----------|
-|1615      |
-*/
-
-SELECT
-	*
-FROM
-	orders
-LIMIT
-	5;
-
-/*
-| order_id | customer_id | order_status | order_date | required_date | shipped_date | store_id | staff_id |
-|----------|-------------|--------------|------------|---------------|--------------|----------|----------|
-| 1        | 259         | 4            | 2016-01-01 | 2016-01-03    | 2016-01-03   | 1        | 2        |
-| 2        | 1212        | 4            | 2016-01-01 | 2016-01-04    | 2016-01-03   | 2        | 6        |
-| 3        | 523         | 4            | 2016-01-02 | 2016-01-05    | 2016-01-03   | 2        | 7        |
-| 4        | 175         | 4            | 2016-01-03 | 2016-01-04    | 2016-01-05   | 1        | 3        |
-| 5        | 1324        | 4            | 2016-01-03 | 2016-01-06    | 2016-01-06   | 2        | 6        |
-*/
-
--- order_items' table
-SELECT
-	COUNT(*) AS num_order_items
-FROM
-	order_items;
-
-/*
-|num_order_items|
-|---------------|
-|4722           |
-*/
-
-SELECT
-	*
+	orders.store_id,
+    stores.store_name,
+    ROUND(SUM(quantity * list_price * (1 - discount)), 2) AS total_sales
 FROM
 	order_items
-LIMIT
-	5;
+LEFT JOIN orders
+	ON order_items.order_id = orders.order_id
+LEFT JOIN stores
+	ON orders.store_id = stores.store_id
+WHERE orders.order_status = 4
+GROUP BY
+	orders.store_id, stores.store_name
+ORDER BY total_sales DESC;
 
 /*
-| order_id | item_id | product_id | quantity | list_price | discount |
-|----------|---------|------------|----------|------------|----------|
-| 1        | 1       | 20         | 1        | 599.99     | 0.20     |
-| 1        | 2       | 8          | 2        | 1799.99    | 0.07     |
-| 1        | 3       | 10         | 2        | 1549.00    | 0.05     |
-| 1        | 4       | 16         | 2        | 599.99     | 0.05     |
-| 1        | 5       | 4          | 1        | 2899.99    | 0.20     |
-
+|store_id|store_name      |total_sales|
+|--------|----------------|-----------|
+|2       |Baldwin Bikes   |4701209.57 |
+|1       |Santa Cruz Bikes|1255491.65 |
+|3       |Rowlett Bikes   |705914.03  |
 */
 
--- stocks' table
+-- *******************************************************	
+
+-- TOTAL SALES PER STORE AND YEAR
 SELECT
-	COUNT(*) AS num_stocks
+	EXTRACT(YEAR FROM order_date) AS year,
+    orders.store_id,
+    stores.store_name,
+    ROUND(SUM(quantity * list_price * (1 - discount)), 2) AS total_sales
 FROM
-	stocks;
+	order_items
+LEFT JOIN orders
+	ON order_items.order_id = orders.order_id
+LEFT JOIN stores
+	ON orders.store_id = stores.store_id
+WHERE orders.order_status = 4
+GROUP BY
+	orders.store_id, stores.store_name, year
+ORDER BY year, total_sales DESC;
 
 /*
-| num_stocks |
-|------------|
-| 939        |
+|year|store_id|store_name      |total_sales|
+|----|--------|----------------|-----------|
+|2016|2       |Baldwin Bikes   |1585135.69 |
+|2016|1       |Santa Cruz Bikes|544659.14  |
+|2016|3       |Rowlett Bikes   |243054.37  |
+|2017|2       |Baldwin Bikes   |2433949.29 |
+|2017|1       |Santa Cruz Bikes|544197.00  |
+|2017|3       |Rowlett Bikes   |368826.36  |
+|2018|2       |Baldwin Bikes   |682124.59  |
+|2018|1       |Santa Cruz Bikes|166635.51  |
+|2018|3       |Rowlett Bikes   |94033.30   |
 */
 
+
+-- •	What is the best-selling product in each store?
+
+WITH RankQuantityPerStore AS (
+	SELECT
+    EXTRACT(YEAR FROM orders.order_date) AS year,
+	products.product_name,
+    category_name,
+    stores.store_name,
+    order_items.quantity,
+    orders.order_status,
+    ROW_NUMBER() OVER(PARTITION BY stores.store_name
+						ORDER BY order_items.quantity DESC) AS quantity_rank
+	FROM
+		stores
+	LEFT JOIN orders
+		ON stores.store_id = orders.store_id
+	LEFT JOIN order_items
+		ON orders.order_id = order_items.order_id
+	LEFT JOIN products
+		ON order_items.product_id = products.product_id
+	LEFT JOIN categories
+		ON products.category_id = categories.category_id
+	WHERE orders.order_status = 4
+	ORDER BY order_items.quantity
+)
 SELECT
-	*
-FROM
-	stocks
-LIMIT
-	5;
+	year,
+    store_name,
+    product_name,
+    category_name,
+    quantity
+FROM RankQuantityPerStore
+WHERE quantity_rank = 1;
 
 /*
-| store_id | product_id | quantity |
-|----------|------------|----------|
-| 1        | 1          | 27       |
-| 1        | 2          | 5        |
-| 1        | 3          | 6        |
-| 1        | 4          | 23       |
-| 1        | 5          | 22       |
+|year|store_name      |product_name                                 |category_name      |quantity|
+|----|----------------|---------------------------------------------|-------------------|--------|
+|2016|Baldwin Bikes   |Electra Girl's Hawaii 1 (20-inch) - 2015/2016|Children Bicycles  |2       |
+|2016|Rowlett Bikes   |Surly Straggler 650b - 2016                  |Cyclocross Bicycles|2       |
+|2017|Santa Cruz Bikes|Electra Amsterdam Fashion 7i Ladies' - 2017  |Cruisers Bicycles  |2       |
 */
 
--- customers' table
+-- ***********************************************************************************
+
+WITH RankQuantityPerStore AS (
+	SELECT
+	EXTRACT(YEAR FROM orders.order_date) AS year,
+    products.product_name,
+	category_name,
+    stores.store_name,
+    order_items.quantity,
+    ROW_NUMBER() OVER(PARTITION BY EXTRACT(YEAR FROM orders.order_date), stores.store_name
+						ORDER BY order_items.quantity DESC) AS quantity_row_number
+	FROM
+		stores
+	LEFT JOIN orders
+		ON stores.store_id = orders.store_id
+	LEFT JOIN order_items
+		ON orders.order_id = order_items.order_id
+	LEFT JOIN products
+		ON order_items.product_id = products.product_id
+	LEFT JOIN categories
+		ON products.category_id = categories.category_id
+	WHERE orders.order_status = 4)
 SELECT
-	COUNT(*) AS num_customers
+	year,
+    store_name,
+    product_name,
+    category_name,
+    quantity
 FROM
-	customers;
-    
+	RankQuantityPerStore
+WHERE quantity_row_number = 1;
+
 /*
-|num_customers|
-|-------------|
-|1445         |
+|year|store_name      |product_name                        |category_name      |quantity|
+|----|----------------|------------------------------------|-------------------|--------|
+|2016|Baldwin Bikes   |Trek Fuel EX 8 29 - 2016            |Mountain Bikes     |2       |
+|2016|Rowlett Bikes   |Electra Townie Original 7D EQ - 2016|Comfort Bicycles   |2       |
+|2016|Santa Cruz Bikes|Surly Straggler 650b - 2016         |Cyclocross Bicycles|2       |
+|2017|Baldwin Bikes   |Trek Conduit+ - 2016                |Electric Bikes     |2       |
+|2017|Rowlett Bikes   |Surly Steamroller - 2017            |Road Bikes         |2       |
+|2017|Santa Cruz Bikes|Trek Powerfly 8 FS Plus - 2017      |Electric Bikes     |2       |
+|2018|Baldwin Bikes   |Trek Powerfly 8 FS Plus - 2017      |Electric Bikes     |2       |
+|2018|Rowlett Bikes   |Trek Super Commuter+ 8S - 2018      |Electric Bikes     |2       |
+|2018|Santa Cruz Bikes|Sun Bicycles Lil Kitt'n - 2017      |Children Bicycles  |2       |
 */
 
+
+
+-- •	Which stores have the highest average purchase ticket?
+
+WITH AmountProduct AS (
+	SELECT
+		orders.order_id,
+		orders.order_status,
+		order_items.item_id,
+		order_items.product_id,
+		stores.store_name,
+		(quantity * list_price * (1 - discount)) AS amount  
+	FROM
+		orders
+	LEFT JOIN order_items
+		ON orders.order_id = order_items.order_id
+	LEFT JOIN stores
+		ON orders.store_id = stores.store_id
+	WHERE order_status = 4)
 SELECT
-	*
+    store_name,
+    ROUND(AVG(amount), 2) AS AVG_purchase_ticket
 FROM
-	customers
-LIMIT
-	5;
+	AmountProduct
+GROUP BY store_name
+ORDER BY AVG_purchase_ticket DESC;
 
 /*
-|customer_id|first_name|last_name|phone         |email                  |street               |city         |state|zip_code|
-|-----------|----------|---------|--------------|-----------------------|---------------------|-------------|-----|--------|
-|1          |Debra     |Burks    |              |debra.burks@yahoo.com  |9273 Thorne Ave.     |Orchard Park |NY   |14127   |
-|2          |Kasha     |Todd     |              |kasha.todd@yahoo.com   |910 Vine Street      |Campbell     |CA   |95008   |
-|3          |Tameka    |Fisher   |              |tameka.fisher@aol.com  |769C Honey Creek St. |Redondo Beach|CA   |90278   |
-|4          |Daryl     |Spence   |              |daryl.spence@aol.com   |988 Pearl Lane       |Uniondale    |NY   |11553   |
-|5          |Charolette|Rice     |(916) 381-6003|charolette.rice@msn.com|107 River Dr.        |Sacramento   |CA   |95820   |
+|store_name      |AVG_purchase_ticket|
+|----------------|-------------------|
+|Rowlett Bikes   |1637.85            |
+|Baldwin Bikes   |1585.57            |
+|Santa Cruz Bikes|1534.83            |
 */
+	
 
--- staffs' table
-SELECT
-	COUNT(*) AS num_staff
-FROM
-	staffs;
 
-/*
-| num_staff |
-|-----------|
-| 10        |
-*/
-
-SELECT
-	*
-FROM
-	staffs;
-    
-/*
-|staff_id|first_name|last_name|email                        |phone         |active|store_id|manager_id|
-|--------|----------|---------|-----------------------------|--------------|------|--------|----------|
-|1       |Fabiola   |Jackson  |fabiola.jackson@bikes.shop   |(831) 555-5554|1     |1       |          |
-|2       |Mireya    |Copeland |mireya.copeland@bikes.shop   |(831) 555-5555|1     |1       |1         |
-|3       |Genna     |Serrano  |genna.serrano@bikes.shop     |(831) 555-5556|1     |1       |2         |
-|4       |Virgie    |Wiggins  |virgie.wiggins@bikes.shop    |(831) 555-5557|1     |1       |2         |
-|5       |Jannette  |David    |jannette.david@bikes.shop    |(516) 379-4444|1     |2       |1         |
-|6       |Marcelene |Boyer    |marcelene.boyer@bikes.shop   |(516) 379-4445|1     |2       |5         |
-|7       |Venita    |Daniel   |venita.daniel@bikes.shop     |(516) 379-4446|1     |2       |5         |
-|8       |Kali      |Vargas   |kali.vargas@bikes.shop       |(972) 530-5555|1     |3       |1         |
-|9       |Layla     |Terrell  |layla.terrell@bikes.shop     |(972) 530-5556|1     |3       |7         |
-|10      |Bernardine|Houston  |bernardine.houston@bikes.shop|(972) 530-5557|1     |3       |7         |
-*/
+-- •	Which stores show the highest month-over-month sales growth?
+-- •	What percentage of total sales comes from each store?
